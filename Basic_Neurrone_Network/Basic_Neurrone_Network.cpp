f67a9ff451fd504fe68b6746043068300483a72d
LucLenne <113776725+LucLenne@users.noreply.github.com>
2025-03-07 19:11:10 +0100

    advance

--- Basic_Neurrone_Network/Basic_Neurrone_Network.cpp
+++ Basic_Neurrone_Network/Basic_Neurrone_Network.cpp
@@ -12,48 +12,77 @@ public:
 		srand(time(0));
 		return Eigen::Vector3d(rand() % 256, rand() % 256, rand() % 256);
 	}
-	bool IsGreen(Eigen::Vector3d pixel)
-	{
-
-	}
 };
 
 
 class Perceptron
 {
 private:
+	Pixel pixel;
 	Eigen::Vector3d entryMatrix;
 	Eigen::Vector3d weights = {0.1,0.1,0.1};
+	double learningRate;
+	Eigen::Vector3d greenPixel;
 
 
-	double ScalarProduct()
+	double ScalarProduct(Eigen::Vector3d entry)
 	{
-		return entryMatrix.dot(weights);
+		return entry.dot(weights);
 	}
 
 	bool ActivationFunction(double weightedSum)
 	{
-		return weightedSum > 100;
+		return weightedSum > 0.8;
+	}
+
+	void CalculateError() 
+	{
+		double error = ScalarProduct(greenPixel) - ScalarProduct(entryMatrix);
+		for (int i = 0 ; i < 3 ; i++) 
+		{
+			weights[i] += learningRate * error * entryMatrix[i];
+			std::cout << weights[i] << std::endl;
+		}
+		entryMatrix = NormalizeValueVector(pixel.RandomPixel());
+	}
+
+	Eigen::Vector3d NormalizeValueVector(Eigen::Vector3d vectorToNormalize)
+	{
+		return vectorToNormalize / 255;
 	}
 
 public:
-	Perceptron(Eigen::Vector3d entry)
+	Perceptron(float pLearningRate)
 	{
-		entryMatrix = entry;
+		learningRate = pLearningRate;
+		greenPixel = NormalizeValueVector({ 0,255,0 });
 	}
 
-	void Predict()
+	void TrainPerceptron(int numberIteration)
 	{
-		std::cout << ActivationFunction(ScalarProduct());
+		for (int i = 0; i < numberIteration; i++) 
+		{
+			CalculateError();
+		}
+	}
+
+	Eigen::Vector3d PrintWeights()
+	{
+		return weights;
+	}
+
+	bool Prediction(Eigen::Vector3d entry) 
+	{
+		return ActivationFunction(ScalarProduct(entry));
 	}
 };
 
 
 int main()
 {
-	Perceptron bGreen({ 0.1,0.5,0.9 });
-	bGreen.Predict();
-	Pixel pixel;
-	pixel.RandomPixel();
+	Perceptron perceptron(0.01f);
+	perceptron.TrainPerceptron(100);
+	std::cout << perceptron.PrintWeights() << std::endl;
+	std::cout << perceptron.Prediction({ 0,255,0 });
 }
 
